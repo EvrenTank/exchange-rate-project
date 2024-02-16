3268c2fd3ef5a3f0f358da7cef3b72869d8d82ae
EvrenTank <evrentank@gmail.com>
2024-02-16 20:02:53 +0300

    some changes

--- components/menu/icon.tsx
+++ components/menu/icon.tsx
@@ -1,8 +1,20 @@
-import sytles from "../../styles/menu/icon.module.css";
-const Icon = () => {
+'use client';
+import styles from "../../styles/menu/icon.module.css";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import { useState } from "react";
+
+const Icon = ({open,setOpen}:any) => {
+
     return (
-        <div>
-            <img src="money.png" className={sytles.image}></img>
+        <div className={styles.container}>
+            <MenuRoundedIcon className={styles.menuIcon} 
+            onClick={()=>{setOpen((prevState:any) => !prevState)}} />
+            <div className={styles.div2}>
+            <img src="money.png" className={styles.image} title="Exchange Rate"></img>
+            <h2 className={styles.heading} 
+            style={{
+                display: open ? "inline" : "none",
+            }} >Exchange Rate</h2></div>
         </div>
     )
 }
--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -1,34 +1,53 @@
 "use client";
 import styles from "../../styles/menu/menu.module.css";
 import Icon from "./icon";
+import HomeIcon from '@mui/icons-material/Home';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import HelpCenterSharpIcon from '@mui/icons-material/HelpCenterSharp';
 import { useState } from "react";
 const Menu = () => {
  
     const [selectedpage,setSelectedpage] = useState<string>("Ana Sayfa");
+    const [open,setOpen] = useState<boolean>(false);
 
     return (
         <div className={styles.menuDiv}>
-            <Icon></Icon>
+
+            <Icon open={open} setOpen ={setOpen} ></Icon>
             <ul className={styles.list}>
                 <li style={{
-                    color: selectedpage == "Ana Sayfa" ? "blue" : "black"
+                    color: selectedpage == "Ana Sayfa" ? "orange" : "rgb(200,200,200)"
                 }} 
                 onClick={()=>{
                     setSelectedpage("Ana Sayfa");
                 }}
-                >Ana Sayfa</li>
+                ><HomeIcon style={{
+                    fontSize: open ? "30px" : "40px",
+                }} />
+                <p style={{
+                    display: open ? " inline" : "none" 
+                }}>Ana Sayfa</p></li>
                 <li style={{
-                    color: selectedpage == "İkinci Sayfa" ? "blue" : "black"
+                    color: selectedpage == "Dönüştür" ? "orange" : "rgb(200,200,200)"
                 }} 
-                                onClick={()=>{
-                                    setSelectedpage("İkinci Sayfa");
-                                }}>İkinci Sayfa</li>
+                onClick={()=>{
+                    setSelectedpage("Dönüştür");
+                }}>
+                <CalculateIcon style={{
+                    fontSize: open ? "30px" : "40px",
+                }}/><p style={{
+                    display: open ? " inline" : "none" 
+                }} >Dönüştür</p></li>
                 <li style={{
-                    color: selectedpage == "Hakkında" ? "blue" : "black"
+                    color: selectedpage == "Hakkında" ? "orange" : "rgb(200,200,200)"
                 }} 
-                                onClick={()=>{
-                                    setSelectedpage("Hakkında");
-                                }}>Hakkında</li>
+                onClick={()=>{setSelectedpage("Hakkında");}}>
+                <HelpCenterSharpIcon style={{
+                    fontSize: open ? "30px" : "40px",
+                }}/>
+                <p style={{
+                    display: open ? " inline" : "none" 
+                }}>Hakkında</p></li>
             </ul>
 
         </div>
